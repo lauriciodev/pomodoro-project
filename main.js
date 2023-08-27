fdead073ecb65363026346e476d424e650c2e9ee
lauriciodev <developerwebmasterpro@gmail.com>
2023-08-27 11:29:04 -0300

    versão 0.0.1 inicial

---
+++ main.js
@@ -0,0 +1,79 @@
+class Pomodoro {
+  constructor(prod, free) {
+    this.timeProd = prod;
+    this.freeTime = free;
+    this.isRunning = true;
+    this.seconds = 60;
+    this.element = document.getElementById("time");
+    this.elementStatus = document.getElementById("status");
+    this.songFree = document.getElementById("free");
+    this.songStart = document.getElementById("start");
+  }
+
+  showTime(minutes, seconds) {
+    if (this.isRunning) {
+      this.elementStatus.innerHTML = "PRODUZIR";
+      this.elementStatus.style.backgroundColor = "#4a6ec1";
+    } else {
+      this.elementStatus.innerHTML = "DESCANÇAR";
+      this.elementStatus.style.backgroundColor = "green";
+    }
+    this.element.innerHTML = `${minutes < 10 ? "0" + minutes : minutes} :  ${
+      seconds < 10 ? "0" + seconds : seconds
+    }`;
+  }
+
+  startProd(time) {
+    time = time - 1;
+    var prodInter = setInterval(() => {
+      this.seconds--;
+      if (this.seconds === 0) {
+        if (time === 0 && this.seconds === 0) {
+          this.isRunning = false;
+          clearInterval(prodInter);
+          this.start();
+          this.seconds = 60;
+        }
+        time--;
+        this.seconds = 60;
+        return;
+      }
+      this.showTime(time, this.seconds);
+    }, 1000);
+  }
+
+  startFree(time) {
+    time = time - 1;
+
+    var freeInter = setInterval(() => {
+      this.seconds--;
+      if (this.seconds === 0) {
+        if (time === 0 && this.seconds === 0) {
+          this.isRunning = true;
+          clearInterval(freeInter);
+          this.start();
+          this.seconds = 3;
+        }
+        time--;
+        this.seconds = 3;
+        return;
+      }
+      this.showTime(time, this.seconds);
+    }, 1000);
+  }
+
+  start() {
+    if (this.isRunning) {
+      this.startProd(this.timeProd);
+      this.songStart.play();
+
+      return;
+    }
+
+    this.startFree(this.freeTime);
+    this.songFree.play();
+  }
+}
+
+const pomo = new Pomodoro(1, 1);
+pomo.start();
