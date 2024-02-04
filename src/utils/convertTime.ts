a3473968ee005c7c681cc0aeb772f5d7aeec6458
lauriciodev <developerwebmasterpro@gmail.com>
2024-02-04 11:51:32 -0300

    convert hours

--- src/utils/convertTime.ts
+++ src/utils/convertTime.ts
@@ -1,6 +1,7 @@
 export function convertTime(time:number): string{
   const zeroLeft = (n:number) => Math.floor(n).toString().padStart(2,"0");
+  const hor = zeroLeft(time / 3600); 
   const min = zeroLeft((time / 60) % 60); 
   const sec = zeroLeft((time % 60) % 60); 
- return `${min}:${sec}`
+ return `${hor}:${min}:${sec}`
 }
